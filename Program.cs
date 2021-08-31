1564a178db00147d62f89bc333f73afb841b415b
Sanier <Sanier20@mail.ru>
2021-08-31 19:36:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Objective_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Alexandr";
+            byte MyAge = 21;
+            bool HaveIApet = true;
+            double MyShoeSize = 41;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("My age " + MyAge);
+            Console.WriteLine("Do I have a pet " + HaveIApet);
+            Console.WriteLine("My shot size is " + MyShoeSize);
+        }
+    }
+}
